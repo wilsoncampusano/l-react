497a7b8ccce48b89a858cd4939b43b781b9d31c4
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2018-09-29 16:52:07 -0400

    stateless titles

--- weather-app/src/components/Titles.js
+++ weather-app/src/components/Titles.js
@@ -1,16 +1,10 @@
 import React from 'react';
 
-class Titles extends React.Component {
-
-  render(){
-    return(
-      <div>
-        <h1>Weather App</h1>
-        <p>Find out conditions and more...</p>
-      </div>
-    );
-  }
-}
-
+const Titles = () => (
+  <div>
+    <h1>Weather App</h1>
+    <p>Find out conditions and more...</p>
+  </div>
+);
 
 export default Titles
