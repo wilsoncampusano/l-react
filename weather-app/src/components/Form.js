10168cfaed0f37b8eb21e740a6a466318246144f
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2018-09-29 16:51:08 -0400

    stateless form

--- weather-app/src/components/Form.js
+++ weather-app/src/components/Form.js
@@ -1,20 +1,15 @@
 import React from 'react';
 
 
-class Form extends React.Component {
-
-  render(){
-    return(
-      <div>
-        <form onSubmit={this.props.getWeather}>
-          <input name='city' placeholder='City...'></input>
-          <input name='country' placeholder='Country...'></input>
-          <button >Weather</button>
-        </form>
-      </div>
-    );
-  }
-}
+const Form = props => (
+  <div>
+    <form onSubmit={props.getWeather}>
+      <input name='city' placeholder='City...'></input>
+      <input name='country' placeholder='Country...'></input>
+      <button >Weather</button>
+    </form>
+  </div>
+);
 
 
 
