f37e7a25bbf18b592e04ee6da98299b115010ae6
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2018-09-29 13:23:07 -0400

    form component

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 
 import Titles from './components/Titles'
+import Form from './components/Form'
 
 class App extends React.Component {
 
   render(){
     return(
-      <Titles/>
+        <div>
+          <Titles/>
+          <Form/>
+        </div>
     );
   }
 }
