84b0311373eb57a45da8b95c955d60e8c14e7ea2
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2018-09-29 16:49:44 -0400

    stateless component

--- weather-app/src/components/Weather.js
+++ weather-app/src/components/Weather.js
@@ -1,18 +1,16 @@
 import React from 'react'
 
 
-const Weather = (props) => {
-  return (
-    <div>
-      { props.city && <p>City: { props.city}</p>}
-      { props.country && <p>Country: { props.country}</p>}
-      { props.humidity && <p>Humidity: { props.humidity}</p>}
-      { props.temperature && <p>Temperature: { props.temperature}</p>}
-      { props.description && <p>Description: { props.description}</p>}
-      <p>{ props.error}</p>
-    </div>
-  );
-}
+const Weather = props =>(
+  <div>
+    { props.city && <p>City: { props.city}</p>}
+    { props.country && <p>Country: { props.country}</p>}
+    { props.humidity && <p>Humidity: { props.humidity}</p>}
+    { props.temperature && <p>Temperature: { props.temperature}</p>}
+    { props.description && <p>Description: { props.description}</p>}
+    <p>{ props.error}</p>
+  </div>
+);
 
 
 export default Weather
