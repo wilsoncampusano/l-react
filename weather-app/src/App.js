4b75fa57afb56a8e15554825dee80e75da0fd1fd
Wilson Campusano <wilsoncampusanojorge@gmail.com>
2018-09-29 13:18:27 -0400

    title component

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 
+import Titles from './components/Titles'
+
 class App extends React.Component {
 
   render(){
     return(
-      <div>Hello!</div>
-
+      <Titles/>
     );
   }
 }
---
+++ weather-app/src/components/Titles.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Titles extends React.Component {
+
+  render(){
+    return(
+      <div>
+        <h1>Weather App</h1>
+        <p>Find out conditions and more...</p>
+      </div>
+    );
+  }
+}
+
+
+export default Titles
