e7e9778287220d90c7e7792f2818d3b32cde29b0
Wilson <wilsoncampusanojorge@gmail.com>
2018-10-04 16:41:14 -0400

    adding navigation

--- router/src/App.js
+++ router/src/App.js
@@ -5,19 +5,22 @@ import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contant';
 import Error from './components/Error';
+import Nav from './components/Nav';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-
-        <Switch>
-              <Route path="/" component= { Home } exact ></Route>
-              <Route path="/contact" component={ Contact }></Route>
-              <Route path="/about" component = { About }></Route>
-              <Route component={ Error }></Route>
-        </Switch>
-
+        
+        <div>
+          <Nav></Nav>
+          <Switch>
+                <Route path="/" component= { Home } exact ></Route>
+                <Route path="/contact" component={ Contact }></Route>
+                <Route path="/about" component = { About }></Route>
+                <Route component={ Error }></Route>
+          </Switch>
+        </div>
       </BrowserRouter>
     );
   }
