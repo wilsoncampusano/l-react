692c049d943c8cbdf321adf45bc36bd88871affb
Wilson <wilsoncampusanojorge@gmail.com>
2018-10-12 10:33:14 -0400

    rendering

--- docsconcepts/src/App.js
+++ docsconcepts/src/App.js
@@ -2,33 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function formatName(user){
-    return user.firstName + ' ' + user.lastName
-}
-
-
-const user = {
-  firstName: 'Harper',
-  lastName: 'Perez',
-  avatarId: "1234567890"
-};
-
-function greeting(user){
-  if (user){
-    return element
-  }else {
-    return <h1>Hello, Stranger.</h1>
-  }
-}
-
-const element = <div  tabIndex="0"> <h1 data-custom-id={ user.avatarId } >Hello , {formatName(user)}!</h1> </div>
-
 class App extends Component {
   
 
   render() {
+
+    const element =  <div>
+    <h1>Hello, world!</h1>
+    <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div> ;
     return (
-      greeting(user)
+      element 
     );
   }
 }
