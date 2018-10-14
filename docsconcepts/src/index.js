574eb6db59f8d3af9b028fcd3d26f15ae44b3449
Wilson <wilsoncampusanojorge@gmail.com>
2018-10-14 13:58:47 -0400

    doc concepts

--- docsconcepts/src/index.js
+++ docsconcepts/src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function tick(){
-    const element = (<div>
-    <h1>Hello, world!</h1>
-    <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>) ;
 
-    ReactDOM.render(element, document.getElementById('root'));
+function Welcome(properties){
+    return <h1> Hello, {properties.name}!</h1>;
 }
-setInterval(tick, 1000)
+
+
+const element = <Welcome name='M'/>
+
+
+ReactDOM.render(element, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
