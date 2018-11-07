368ea8cb1719ad25e0647e9db7cc660f3a4f4a19
Wilson <wilsoncampusanojorge@gmail.com>
2018-11-07 11:22:03 -0400

    test

--- authorquiz/src/AuthorQuiz.test.js
+++ authorquiz/src/AuthorQuiz.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AuthorQuiz from 'AuthorQuiz'
+import AuthorQuiz from './AuthorQuiz'
+
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<AuthorQuiz />, div);
